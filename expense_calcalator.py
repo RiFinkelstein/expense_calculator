803d5eca2375c24d05ca622773ffdf504fde5ae4
riFinkelstein <rivkafinkelstein823@gmail.com>
2023-03-18 22:32:46 +0200

    corrected code to make work

--- expense_calcalator.py
+++ expense_calcalator.py
@@ -2,7 +2,7 @@ import os
 
 
 class Budget(object):
-    def _init_(self):
+    def __init__(self):
         os.system('cls')
         self.budget = float(input("how much is your budget\n"))
         self.spending = self.budget * 0.5
@@ -11,7 +11,7 @@ class Budget(object):
     def main(self):
         os.system('cls')
         print("this calculator follows the 50-20-30 budget rule.\n")
-        print("yourtotal budget is $", '{: .2f}'.self.budget)
+        print("yourtotal budget is $", '{:.2f}'.format(self.budget))
         main_option = int(input(
             '\n what do you want to do? \n 1) View ovrall budget \n2)View spending budger?'))
         if main_option == 1:
@@ -19,11 +19,11 @@ class Budget(object):
         elif main_option == 2:
             self.spending_budget()
         else:
-            quit
+            quit()
 
     def overall_budget(self):
         os.system('cls')
-        option = int(input("how muhc do you want to save? 1)2% 2)30%"))
+        option = int(input("how much do you want to save? 1)2% 2)30%"))
         if option == 1:
             self.saving = 0.2
         elif option == 2:
@@ -37,14 +37,16 @@ class Budget(object):
         os.system('pause')
         self.main()
 
+    def spending_budget(self):
+        os.system('cls')
+        print('Spending Budget: $', '{:.2f}'. format(self.spending))
+        rent = float(input('\n how much rent do you pay monthly?\n'))
+        bills = float(input('\n how much are your monthly bills?\n'))
+        groceries = self.spending-rent-bills
+        print('\nEXPENSES: \n Rent: $', '{:.2f}'.format(rent), '\nbills:$', '{:.2f}'.format(
+            bills), '\nGroceries''{:.2f}'.format(groceries),)
+        os.system('pause')
+        self.main()
 
-def speding_budget(self):
-    os.system('cls')
-    print('Spending Budger: $', '{:.2f}'. format(self.spending))
-    rent = float(input('\n how much rent do you pay monthly?\n'))
-    bills = float(input('\n how much are your monthly bills?\n'))
-    groceries = self.spending-rent-bills
-    print('\nEXPENSES: \n Rent: $', '{:.2f}'.format(rent), '\nbills:$', '{:.2f}'.format(
-        bills), '\nGroceries''{:.2f}'.format(groceries),)
-    os.system('pause')
-    self.main()
+
+Budget()
